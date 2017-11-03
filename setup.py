01fa43d577f1c07cdbcdc53f8827a4d6ac4c87f1
Cameron Curry <cameroncurry@users.noreply.github.com>
2017-11-03 22:49:00 +0000

    added setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,20 @@
+#
+# Copyright Cameron Curry (c) 2017
+#
+
+from setuptools import setup, find_packages
+from questrade import __version__
+
+install_requires = [
+    'requests==2.18.4'
+]
+
+setup(
+    name='questrade',
+    version=__version__,
+    author="Cameron Curry",
+    description='Python Client for Questrade API',
+    install_requires=install_requires,
+    packages=find_packages(),
+    test_suite='tests'
+)
